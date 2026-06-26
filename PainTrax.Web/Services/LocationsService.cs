80ba63b3e113d219c40c15d79c08453e0f0ac67f
saranya-1991 <ssaranyamca15@gmail.com>
2026-06-26 17:50:46 +0530

    Completed location chnages

--- PainTrax.Web/Services/LocationsService.cs
+++ PainTrax.Web/Services/LocationsService.cs
@@ -10,7 +10,19 @@ namespace PainTrax.Web.Services
     {
         public List<tbl_locations> GetAll(string cnd = "")
         {
-            string query = "select * from tbl_locations where 1=1 and isactive=1 ";
+            string query = "select * from tbl_locations where 1=1 and isactive=1 ";           
+
+            if (!string.IsNullOrEmpty(cnd))
+                query = query + cnd;
+
+
+            List<tbl_locations> dataList = ConvertDataTable<tbl_locations>(GetData(query));
+            return dataList;
+        }
+        public List<tbl_locations> GetAllGrid(string cnd = "")
+        {
+            //string query = "select * from tbl_locations where 1=1 and isactive=1 ";
+            string query = "select * from tbl_locations where 1=1";
 
             if (!string.IsNullOrEmpty(cnd))
                 query = query + cnd;
